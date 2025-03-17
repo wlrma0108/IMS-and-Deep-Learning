9749bc0eb7b063cf690a9898ea30d97a4fe425f0
wlrma0108 <dlwlrma0108@gmail.com>
2025-03-17 22:28:14 +0900

    visualize

--- data processing.py
+++ data processing.py
@@ -1,1 +1,27 @@
-class imagereshape:
+def visualize(image_batch, mask_batch=None, pred_batch=None, num_samples=8, hot_encode=True):
+    num_classes = mask_batch.shape[-1] if mask_batch is not None else 0
+    fix, ax = plt.subplots(num_classes + 1, num_samples, figsize=(num_samples * 2, (num_classes + 1) * 2))
+
+    for i in range(num_samples):
+        ax_image = ax[0, i] if num_classes > 0 else ax[i]
+        if hot_encode: 
+            ax_image.imshow(image_batch[i,:,:,0], cmap='Greys')
+        else: 
+            ax_image.imshow(image_batch[i,:,:])
+        ax_image.set_xticks([]) 
+        ax_image.set_yticks([])
+        
+        if mask_batch is not None:
+            for j in range(num_classes):
+                if pred_batch is None:
+                    mask_to_show = mask_batch[i,:,:,j]
+                else:
+                    mask_to_show = np.zeros(shape=(*mask_batch.shape[1:-1], 3)) 
+                    mask_to_show[..., 0] = pred_batch[i,:,:,j] > 0.5
+                    mask_to_show[..., 1] = mask_batch[i,:,:,j]
+                ax[j + 1, i].imshow(mask_to_show, vmin=0, vmax=1)
+                ax[j + 1, i].set_xticks([]) 
+                ax[j + 1, i].set_yticks([]) 
+
+    plt.tight_layout()
+    plt.show()
